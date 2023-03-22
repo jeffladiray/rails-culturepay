4385ee460b52f26cf4deafc05f4547054dddb116
Jeff LADIRAY <ladirayjeff@gmail.com>
2023-03-22 16:28:15 +0100

    fix: fix

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module Culturepay
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
+    config.load_defaults 6.1
+    config.autoloader = :classic
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
